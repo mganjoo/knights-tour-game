94d505b8763aa94ae4ccdf626788378721fa5329
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-04-28 13:03:46 -0700

    Test without needing chess.js

--- src/ChessLogic.test.ts
+++ src/ChessLogic.test.ts
@@ -1,34 +1,41 @@
 import { getPuzzleFen, SQUARES } from "./ChessLogic"
-import * as ChessJS from "chess.js"
 import * as fc from "fast-check"
+import { Chessground } from "chessground"
 
-const Chess = typeof ChessJS === "function" ? ChessJS : ChessJS.Chess
-
-test("generated puzzle FEN correctly places knight and queen squares", () => {
-  fc.assert(
-    fc.property(
-      fc
-        .tuple(fc.constantFrom(...SQUARES), fc.constantFrom(...SQUARES))
-        .filter((t) => t[0] !== t[1]),
-      ([knightSquare, queenSquare]) => {
-        const chess = new Chess(getPuzzleFen(knightSquare, queenSquare))
-        expect(chess.get(knightSquare)).toStrictEqual({
-          type: "n",
-          color: "w",
-        })
-        expect(chess.get(queenSquare)).toStrictEqual({
-          type: "q",
-          color: "b",
-        })
-      }
+describe("getPuzzleFen()", () => {
+  test("generates correct puzzle FEN with knight and queen", () => {
+    fc.assert(
+      fc.property(
+        fc
+          .tuple(fc.constantFrom(...SQUARES), fc.constantFrom(...SQUARES))
+          .filter((t) => t[0] !== t[1]),
+        ([knightSquare, queenSquare]) => {
+          // dummy element for Chessground to use
+          const wrapper = document.createElement("div")
+          const chessground = Chessground(wrapper, {
+            fen: getPuzzleFen(knightSquare, queenSquare),
+          })
+          // verify placement of pieces using Chessground internal state
+          expect(chessground.state.pieces.get(knightSquare)).toStrictEqual({
+            role: "knight",
+            color: "white",
+          })
+          expect(chessground.state.pieces.get(queenSquare)).toStrictEqual({
+            role: "queen",
+            color: "black",
+          })
+          chessground.destroy()
+          wrapper.remove()
+        }
+      )
     )
-  )
-})
+  })
 
-test("no FEN is generated if both knight and queen have same square", () => {
-  fc.assert(
-    fc.property(fc.constantFrom(...SQUARES), (square) => {
-      expect(getPuzzleFen(square, square)).toBeUndefined()
-    })
-  )
+  test("does not generate FEN if both knight and queen have same square", () => {
+    fc.assert(
+      fc.property(fc.constantFrom(...SQUARES), (square) => {
+        expect(getPuzzleFen(square, square)).toBeUndefined()
+      })
+    )
+  })
 })
