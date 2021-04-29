36334968054fa6841fce43ccab203f2bab88ee4f
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-04-29 11:35:01 -0700

    Add visited squares to visited array

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 import Board from "./Board"
 import { Square } from "./ChessLogic"
+import { Set as ImmutableSet } from "immutable"
 
 const QUEEN_SQUARE: Square = "d5"
 const STARTING_KNIGHT_SQUARE: Square = "h8"
 
 const App: React.FC = () => {
+  const [visitedSquares, setVisitedSquares] = useState<ImmutableSet<Square>>(
+    ImmutableSet([STARTING_KNIGHT_SQUARE])
+  )
+  const handleMove = (start: Square, end: Square) => {
+    setVisitedSquares(visitedSquares.add(end))
+  }
   return (
     <main className="max-w-xl mx-auto py-6 px-4">
       <Board
         initialKnightSquare={STARTING_KNIGHT_SQUARE}
         queenSquare={QUEEN_SQUARE}
+        checkedSquares={visitedSquares}
+        onKnightMove={handleMove}
       />
     </main>
   )
--- src/Board.tsx
+++ src/Board.tsx
@@ -5,15 +5,24 @@ import { Api as ChessgroundApi } from "chessground/api"
 import * as cg from "chessground/types"
 import { Config } from "chessground/config"
 import { getPuzzleFen, getKnightDests, Square } from "./ChessLogic"
+import { Set as ImmutableSet } from "immutable"
 
 type BoardProps = {
   initialKnightSquare: Square
   queenSquare: Square
+  onKnightMove?: (from: Square, to: Square) => void
+  checkedSquares?: ImmutableSet<Square>
 }
+
+// "check" from https://heroicons.com/
+const CHECK_SVG =
+  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 0 1 0 1.414l-8 8a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 1.414-1.414L8 12.586l7.293-7.293a1 1 0 0 1 1.414 0z" clip-rule="evenodd"/></svg>'
 
 const Board: React.FC<BoardProps> = ({
   initialKnightSquare,
   queenSquare,
+  onKnightMove,
+  checkedSquares,
   children,
 }) => {
   const el = useRef<HTMLDivElement>(null)
@@ -31,11 +40,19 @@ const Board: React.FC<BoardProps> = ({
   const handleMove = useCallback(
     (orig: cg.Key, dest: cg.Key) => {
       const validDests = dests.get(orig)
-      if (validDests && validDests.includes(dest) && dest !== "a0") {
+      if (
+        validDests &&
+        validDests.includes(dest) &&
+        orig !== "a0" &&
+        dest !== "a0"
+      ) {
         setKnightSquare(dest)
+        if (onKnightMove) {
+          onKnightMove(orig, dest)
+        }
       }
     },
-    [dests]
+    [dests, onKnightMove]
   )
   const config: Config = useMemo(
     () => ({
@@ -63,17 +80,15 @@ const Board: React.FC<BoardProps> = ({
       },
       drawable: {
         enabled: false,
-        autoShapes: [
-          {
-            orig: "h8",
-            customSvg:
-              // "check" from https://heroicons.com/
-              '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 0 1 0 1.414l-8 8a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 1.414-1.414L8 12.586l7.293-7.293a1 1 0 0 1 1.414 0z" clip-rule="evenodd"/></svg>',
-          },
-        ],
+        autoShapes: checkedSquares
+          ? Array.from(checkedSquares).map((s) => ({
+              orig: s,
+              customSvg: CHECK_SVG,
+            }))
+          : [],
       },
     }),
-    [fen, dests, handleMove, knightSquare]
+    [fen, dests, handleMove, knightSquare, checkedSquares]
   )
 
   useEffect(() => {
