802ddeb52dc26dca99da75916bf13247f86c3483
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-04-28 21:11:37 -0700

    Tweak piece z-indices

--- src/Board.tsx
+++ src/Board.tsx
@@ -64,10 +64,10 @@ const Board: React.FC<BoardProps> = ({
         enabled: false,
         autoShapes: [
           {
-            orig: "e4",
+            orig: "h8",
             customSvg:
               // "check" from https://heroicons.com/
-              '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#D97706"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 13 4 4L19 7"/></svg><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#D97706"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 13 4 4L19 7"/></svg>',
+              '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 0 1 0 1.414l-8 8a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 1.414-1.414L8 12.586l7.293-7.293a1 1 0 0 1 1.414 0z" clip-rule="evenodd"/></svg>',
           },
         ],
       },
