435857915b85b62824d65d8199037c6a238f469a
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-05-01 12:37:35 -0700

    Remove icon from scoreboard

--- src/Scoreboard.tsx
+++ src/Scoreboard.tsx
@@ -1,31 +1,21 @@
-import classNames from "classnames"
 import React from "react"
 
 interface ScoreboardProps {
   tickers: {
     label: string
     value?: string | number
-    icon?: React.ComponentType<React.ComponentProps<"svg">>
-    iconColorClass?: string
   }[]
 }
 
 const Scoreboard: React.FC<ScoreboardProps> = ({ tickers }) => {
   return (
     <div className="flex flex-wrap justify-evenly my-3 md:justify-start md:space-x-5">
-      {tickers.map(({ label, value, iconColorClass, ...other }) => (
+      {tickers.map(({ label, value }) => (
         <div
           key={label}
           className="flex flex-col-reverse items-center py-1 px-1"
         >
-          <span className="uppercase text-xs flex items-center">
-            {label}
-            {other.icon && (
-              <other.icon
-                className={classNames("h-4 w-4 ml-1", iconColorClass)}
-              />
-            )}
-          </span>
+          <span className="uppercase text-xs flex items-center">{label}</span>
           <span className="text-2xl my-1 md:text-3xl">
             {value !== undefined ? value : "-"}
           </span>
