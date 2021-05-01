fb4c04ba4ab012470a50d0074089ee5889c1c6e7
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-05-01 02:13:24 -0700

    Formatting updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Square, getSquareIncrement, attackedByQueen } from "./ChessLogic"
 import { Set as ImmutableSet } from "immutable"
 import { useConditionalTimeout, useInterval } from "beautiful-react-hooks"
 import Scoreboard from "./Scoreboard"
+import { ChevronDoubleUpIcon } from "@heroicons/react/solid"
 
 const QUEEN_SQUARE: Square = "d5"
 
@@ -41,12 +42,7 @@ const App: React.FC = () => {
   const [visitedSquares, setVisitedSquares] = useState<ImmutableSet<Square>>(
     ImmutableSet()
   )
-  const [targetSquare, setTargetSquare] = useState<Square | undefined>(
-    incrementWhileAttacked(
-      getSquareIncrement(STARTING_KNIGHT_SQUARE, "previous"),
-      "previous"
-    )
-  )
+  const [targetSquare, setTargetSquare] = useState<Square>()
   const [elapsed, setElapsed] = useState(0)
   useInterval(() => {
     if (state === "PLAYING" || state === "KNIGHT_ATTACKED") {
@@ -56,6 +52,12 @@ const App: React.FC = () => {
   const startGame = useCallback(() => {
     setState("PLAYING")
     setVisitedSquares(ImmutableSet([STARTING_KNIGHT_SQUARE]))
+    setTargetSquare(
+      incrementWhileAttacked(
+        getSquareIncrement(STARTING_KNIGHT_SQUARE, "previous"),
+        "previous"
+      )
+    )
   }, [])
   const [numMoves, setNumMoves] = useState(0)
   const handleMove = useCallback(
@@ -74,6 +76,7 @@ const App: React.FC = () => {
         setVisitedSquares(visitedSquares.add(to))
         if (targetSquare === ENDING_KNIGHT_SQUARE) {
           setState("FINISHED")
+          setTargetSquare(undefined)
         } else {
           setTargetSquare(
             incrementWhileAttacked(
@@ -100,9 +103,18 @@ const App: React.FC = () => {
   )
 
   return (
-    <main className="px-4 py-4 max-w-3xl mx-auto md:flex">
+    <main className="px-4 py-4 max-w-4xl mx-auto sm:px-8 sm:py-8 md:flex">
       <div className="md:w-2/3">
-        <div>
+        <div className="mb-4">
+          <h1 className="text-2xl font-semibold text-center mt-4 mb-3">
+            Knight-Queen Tour
+          </h1>
+          <p className="text-sm py-2">
+            Move the knight from one corner of the board to the other in as few
+            moves as possible, avoiding all squares controlled by the queen!
+          </p>
+        </div>
+        <div className="relative mx-1">
           <Board
             state={state}
             knightSquare={knightSquare}
@@ -111,19 +123,35 @@ const App: React.FC = () => {
             targetSquare={targetSquare}
             onKnightMove={handleMove}
           />
+          {state === "NOT_STARTED" && (
+            <div className="absolute inset-0 bg-light-blue-600 backdrop-filter backdrop-blur-lg bg-opacity-50 flex items-center justify-center px-8 py-4">
+              <div className="rounded-2xl bg-white p-3 shadow-2xl border border-gray-400">
+                <div className="flex justify-center">
+                  <button
+                    className="rounded-md border border-blue-300 px-8 py-2 text-sm font-medium shadow-sm text-white bg-light-blue-600 hover:bg-light-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={startGame}
+                  >
+                    Start
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      <div className="md:w-1/3">
-        <div>
-          <button onClick={startGame}>Start</button>
-          <Scoreboard
-            tickers={[
-              { label: "Target", value: targetSquare || "-" },
-              { label: "Moves", value: numMoves },
-              { label: "Time", value: formatSeconds(elapsed) },
-            ]}
-          />
-        </div>
+      <div className="mt-3 py-3 md:w-1/3">
+        <Scoreboard
+          tickers={[
+            {
+              label: "Target",
+              value: targetSquare,
+              icon: ChevronDoubleUpIcon,
+              iconColorClass: "text-yellow-600",
+            },
+            { label: "Moves", value: numMoves },
+            { label: "Time", value: formatSeconds(elapsed) },
+          ]}
+        />
       </div>
     </main>
   )
--- src/Scoreboard.tsx
+++ src/Scoreboard.tsx
@@ -1,19 +1,34 @@
+import classNames from "classnames"
 import React from "react"
 
 interface ScoreboardProps {
   tickers: {
     label: string
-    value: string | number
+    value?: string | number
+    icon?: React.ComponentType<React.ComponentProps<"svg">>
+    iconColorClass?: string
   }[]
 }
 
 const Scoreboard: React.FC<ScoreboardProps> = ({ tickers }) => {
   return (
-    <div className="flex justify-evenly">
-      {tickers.map(({ label, value }) => (
-        <div key={label} className="flex flex-col-reverse items-center">
-          <span className="uppercase text-xs">{label}</span>
-          <span className="text-3xl">{value}</span>
+    <div className="flex flex-wrap justify-evenly">
+      {tickers.map(({ label, value, iconColorClass, ...other }) => (
+        <div
+          key={label}
+          className="flex flex-col-reverse items-center py-1 px-1"
+        >
+          <span className="uppercase text-xs flex items-center">
+            {label}
+            {other.icon && (
+              <other.icon
+                className={classNames("h-4 w-4 ml-1", iconColorClass)}
+              />
+            )}
+          </span>
+          <span className="text-2xl my-1">
+            {value !== undefined ? value : "-"}
+          </span>
         </div>
       ))}
     </div>
