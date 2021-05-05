9b8a4bf6cdeb51118a6c098020e7e9d8bb147126
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-05-05 00:09:27 -0700

    Add confirmation when service worker updates

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,16 @@ ReactDOM.render(
   document.getElementById("root")
 )
 
-serviceWorkerRegistration.register()
+serviceWorkerRegistration.register({
+  onUpdate: () => {
+    const answer = window.confirm(
+      `This website has been updated. Reload to display the latest version?`
+    )
+    if (answer === true) {
+      window.location.reload()
+    }
+  },
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
