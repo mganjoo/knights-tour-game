f27b8e6dd74ae0fa5fee2385114ad7b27ef8e1f9
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-05-16 15:26:51 -0400

    Add more tests for ChessLogic

--- src/ChessLogic.test.ts
+++ src/ChessLogic.test.ts
@@ -4,11 +4,35 @@ import {
   attackedByQueen,
   getKnightDests,
   getPuzzleFen,
+  getPuzzleKnightPath,
+  getShortestKnightPath,
   getSquareIncrement,
+  incrementWhileAttacked,
   isSquare,
   Square,
   SQUARES,
 } from "./ChessLogic"
+
+function toRankFile(square: Square): [number, number] {
+  return [
+    parseInt(square.charAt(1), 10),
+    square.charCodeAt(0) - "a".charCodeAt(0) + 1,
+  ]
+}
+
+function toSquare(rank: number, file: number): Square | undefined {
+  if (1 <= rank && rank <= 8 && 1 <= file && file <= 8) {
+    const maybeSquare =
+      String.fromCharCode("a".charCodeAt(0) + file - 1) + rank.toString()
+    if (maybeSquare && isSquare(maybeSquare)) {
+      return maybeSquare
+    } else {
+      return undefined
+    }
+  } else {
+    return undefined
+  }
+}
 
 describe("getPuzzleFen()", () => {
   test("generates correct puzzle FEN with knight and queen", () => {
@@ -49,27 +73,6 @@ describe("getPuzzleFen()", () => {
     )
   })
 })
-
-function toRankFile(square: Square): [number, number] {
-  return [
-    parseInt(square.charAt(1), 10),
-    square.charCodeAt(0) - "a".charCodeAt(0) + 1,
-  ]
-}
-
-function toSquare(rank: number, file: number): Square | undefined {
-  if (1 <= rank && rank <= 8 && 1 <= file && file <= 8) {
-    const maybeSquare =
-      String.fromCharCode("a".charCodeAt(0) + file - 1) + rank.toString()
-    if (maybeSquare && isSquare(maybeSquare)) {
-      return maybeSquare
-    } else {
-      return undefined
-    }
-  } else {
-    return undefined
-  }
-}
 
 test("attackedByQueen() works for squares vert, diagonally, or horizontally away", () => {
   fc.assert(
@@ -181,4 +184,31 @@ describe("getSquareIncrement()", () => {
       })
     )
   })
+})
+
+test("incrementWhileAttacked() works correctly in both directions", () => {
+  expect(incrementWhileAttacked("f7", "c4", "previousFile")).toEqual("e7")
+  expect(incrementWhileAttacked("e6", "d5", "previousFile")).toEqual("b6")
+  expect(incrementWhileAttacked("c7", "e4", "previousFile")).toEqual("c7")
+
+  expect(incrementWhileAttacked("b4", "d2", "nextFile")).toEqual("c4")
+  expect(incrementWhileAttacked("d5", "e4", "nextFile")).toEqual("g5")
+  expect(incrementWhileAttacked("e8", "d4", "nextFile")).toEqual("e8")
+})
+
+test("getShortestKnightPath() works correctly", () => {
+  expect(getShortestKnightPath("c8", "d5", "d4")).toBeUndefined()
+  expect(getShortestKnightPath("c8", "g5", "d4")?.length).toEqual(6)
+  expect(getShortestKnightPath("h6", "b3", "e5")?.length).toEqual(6)
+  expect(getShortestKnightPath("h6", "g4", "e5")?.length).toEqual(2)
+})
+
+test("getPuzzleKnightPath() works correctly", () => {
+  // prettier-ignore
+  expect(getPuzzleKnightPath("f8", "g7", "d5")).toEqual([
+    "f8", "h7", "f6", "e8", "f6", "h7", "f8", "g6", "e7",
+    "c8", "e7", "g6", "f8", "h7", "f6", "e8", "c7", "a6",
+    "b8", "a6", "c7", "e8", "f6", "h7", "f6", "e8", "g7",
+  ])
+  expect(getPuzzleKnightPath("h8", "a1", "a5")).toBeUndefined()
 })
