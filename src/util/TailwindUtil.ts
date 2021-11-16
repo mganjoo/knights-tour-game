074099a5ecb69ffbbb719aef3f4aa85e8b2d84fa
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-11-16 09:30:33 -0800

    Fix reference to TailwindColorConfig

--- src/util/TailwindUtil.ts
+++ src/util/TailwindUtil.ts
@@ -1,11 +1,11 @@
 import preval from "preval.macro"
 // eslint-disable-next-line
-import { TailwindColorConfig } from "tailwindcss/tailwind-config"
+import { TailwindColorGroup } from "tailwindcss/tailwind-config"
 
 /**
  * Export resolved Tailwind config (such as colors)
  */
-export const colors: TailwindColorConfig = preval`
+export const colors: TailwindColorGroup = preval`
 const myConfig = require("../../tailwind.config.js")
 const resolveConfig = require("tailwindcss/resolveConfig")
 const fullConfig = resolveConfig(myConfig)
