97f8067beacd819b50156fde60622ecd2972b388
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-12-18 11:45:30 -0800

    Add Cypress tests (#29)
    * Remove default tests
    
    * Add Cypress tests

--- cypress/integration/game.ts
+++ cypress/integration/game.ts
@@ -3,10 +3,42 @@ describe("game", () => {
     cy.visit("/")
   })
 
-  it("should start correctly", () => {
-    cy.contains("New game").click()
-    cy.get(".move-dest").last().realClick()
+  it("initializes with empty state", () => {
+    cy.clock()
+    cy.tick(5000)
+    cy.contains("00:00").should("exist")
+    cy.contains("Next square to visit").next().should("contain", "-")
+    cy.contains("Moves").next().should("contain", "0")
+    cy.contains("Squares left").next().should("contain", "35")
   })
 
-  it("makes move correctly", () => {})
+  it("updates timer and next square indicator on start", () => {
+    cy.clock()
+    cy.contains("New game").click()
+    cy.tick(5000)
+    cy.contains("f8").should("be.visible")
+    cy.contains("00:05").should("exist")
+  })
+
+  it("updates move count when knight moves to new square", () => {
+    cy.contains("New game").click()
+    cy.get(".move-dest")
+      .eq(1) // h8 -> g6
+      .realClick()
+      .then(() => {
+        cy.contains("Moves").next().should("contain", "1")
+      })
+  })
+
+  it("shows an error when knight moves to queen-attacked square", () => {
+    cy.contains("End game if knight moves").click()
+    cy.contains("New game").click()
+    cy.get(".move-dest")
+      .eq(0) // h8 -> f7
+      .realClick()
+      .then(() => {
+        cy.contains("Oops, game over").should("exist")
+        cy.contains("Moves").next().should("contain", "1")
+      })
+  })
 })
