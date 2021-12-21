6c1be84f713b32be7a15c66b2739c99cafd112ed
Milind Ganjoo <milind.ganjoo@gmail.com>
2021-12-20 22:04:41 -0800

    Simplify markup of switches and select box (#36)
    * Simplify queen selector
    
    * Clean up switch style

--- src/components/SettingsToggle.tsx
+++ src/components/SettingsToggle.tsx
@@ -15,7 +15,7 @@ const SettingsToggle: React.FC<SettingsToggleProps> = ({
 }) => {
   return (
     <Switch.Group>
-      <div className="flex items-center gap-x-2 md:gap-x-3">
+      <div className="flex items-center gap-x-4">
         <Switch
           checked={enabled}
           onChange={onToggle}
@@ -23,14 +23,14 @@ const SettingsToggle: React.FC<SettingsToggleProps> = ({
             enabled
               ? "bg-blue-700 dark:bg-blue-600"
               : "bg-gray-300 dark:bg-gray-400",
-            "relative inline-flex items-center shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100 dark:focus-visible:ring-offset-gray-900"
+            "inline-flex shrink-0 h-7 w-12 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100 dark:focus-visible:ring-offset-gray-900"
           )}
         >
           <span
             aria-hidden="true"
             className={classNames(
               enabled ? "translate-x-5" : "translate-x-0",
-              "pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow-lg ring-0 transition ease-in-out duration-200"
+              "h-6 w-6 rounded-full bg-white shadow-lg ring-0 transition ease-in-out duration-200"
             )}
           />
         </Switch>
